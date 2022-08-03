120bd3bcccdd300145f855738009725401d4cbb7
Aleksandr Polochkin <alex.polkin@gmail.com>
2022-08-03 17:23:49 +0300

    Создание магазина вынесено в отдельный метод. Добавлены комментарии

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,24 +10,32 @@ public class Main {
     static final int NUMBER_OF_MARKETS = 3;
     static final Market[] markets = new Market[NUMBER_OF_MARKETS];
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         LongAdder revenue = new LongAdder();
 
+        generateMarkets();
+        getReports(revenue);
+
+    }
+
+    static void generateMarkets() {
         for (int i = 0; i < NUMBER_OF_MARKETS; i++) {
             markets[i] = new Market("магазин №" + (i + 1));
             markets[i].start();
         }
 
-        for (Market thread: markets) {
-            thread.join();
+        // Ожидаем окончания работы запущенных потоков
+        try {
+            for (Market thread : markets) {
+                thread.join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
-        getReports(revenue);
-
     }
 
-    static void getReports(LongAdder revenue) throws InterruptedException {
+    static void getReports(LongAdder revenue) {
 
         // В конце рабочего дня запускаем потоки подсчёта выручки по каждому магазину
         final MarketReport[] marketReports = new MarketReport[NUMBER_OF_MARKETS];
@@ -37,8 +45,13 @@ public class Main {
             marketReports[i].start();
         }
 
-        for (MarketReport thread: marketReports) {
-            thread.join();
+        // Ожидаем окончания работы запущенных потоков
+        try {
+            for (MarketReport thread : marketReports) {
+                thread.join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
         System.out.println("Общая выручка: " + revenue.sum());
